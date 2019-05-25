c6d14393d0e9fca608d7899cfee130a432d8de62
GregoireMaison <gregoire.maison@icloud.com>
2019-05-25 12:27:24 +0200

    commit de test

---
+++ src/Application.java
@@ -0,0 +1,27 @@
+class Test {
+
+	private String name;
+	private int number;
+
+	public Test(String name, int number) {
+		// TODO Auto-generated constructor stub
+		this.name = name;
+		this.number = number;
+	}
+
+	public void affiche() {
+		// TODO Auto-generated method stub
+		System.out.println(name + " numéro " + number);
+	}
+	
+}
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test t = new Test("test",1);
+		t.affiche();
+	}
+
+}
